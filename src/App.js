0cff5a10b930f059775af447d650f64253c34677
Ikram Shah <ikram2shah@gmail.com>
2018-12-09 00:07:01 +0530

    Google Maps API
    Design and Implement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  initMap() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+  });
+}
+
+loadMyScript(){
+  let scriptElement = this.createMyMapScript();
+  let scriptsOnPage = document.getElementsByTagName('script');
+  let scripts = scriptsOnPage[0];
+  scripts.parentNode.insertBefore(scriptElement,scripts);
+  window.initMap = this.initMap;
+}
+
+createMyMapScript(){
+  let mapScript = document.createElement("script");
+  const API_KEY = "AIzaSyAssifZMl5AEdnyZK2qjAxSfj7rO4eU97k"
+  mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+  mapScript.async = true;
+  mapScript.defer = true;
+  return mapScript;
+}
+
   render() {
+    this.loadMyScript();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Headers />
+        <Contents />
       </div>
     );
   }
